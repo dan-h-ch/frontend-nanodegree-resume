9cde708d0e1236b63e81547e8c764f4fb668eb94
dan-h-ch <chang.daniel@gmail.com>
2016-06-05 00:06:16 -0700

    release candidate pending content and content style

--- js/helper.js
+++ js/helper.js
@@ -1,20 +1,13 @@
 
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderName = '<h1>%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontact = '<li class="flex-item"><img class="contact-icon" src="%icon%"><span class="contact-text">%value%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="skill-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><img class="skill-icon" src="%icon%"><span class="skill-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployerLink = '<div class="name-text"><a href="%link%">%data%</a></div>';
@@ -22,20 +15,21 @@ var HTMLworkEmployer = '<div class="name-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkTitle = '<div class="position-text">%data%</div>';
 var HTMLworkDates = '<div class="date-text">%data%</div></div>';
-var HTMLworkDescription = '<p>%data%</p><br>';
+var HTMLworkDescription = '<p>%data%</p>';
+var HTMLworkList = '<li>%data%</li>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<div>%data%</div>';
 var HTMLprojectTitleLink = '<div><a href="%link%">%data%</a></div>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p>%data%</p><br>';
+var HTMLprojectDescription = '<p>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<div class="name-text">%data%</div>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolDegree = '<div class="degree-text">%data%';
-var HTMLschoolMajor = ' %data%</div><br>';
+var HTMLschoolMajor = ' %data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
@@ -56,12 +50,45 @@ $(document).ready(function() {
 });
 
 $(function(){
-  $('#nav').hover(function(){
+  $("#nav").hover(function(){
     $(this).animate({width:'250px'},400);
+    $(".category-title").animate({"padding-left":"250px"},400);
+    console.log($(window).width());
+    if ($(window).width() < 1020) {
+      $("#name").animate({"padding-left":"255px"},400);
+      $("#lets-connect").animate({"padding-left":"255px"},400);
+    } else {
+      console.log("didn't move name & lets-connect")
+    }
+
   }, function(){
-    $(this).animate({width:'75px'},70);
-  }).trigger('mouseleave');
+    $(this).animate({width:'75px'},400);
+    $(".category-title").animate({"padding-left":"75px"},400);
+    $("#name").animate({"padding-left":"25%"},400);
+    $("#lets-connect").animate({"padding-left":"25%"},400);
+  }).trigger("load");
 });
+
+$(function(){
+  $("#nav").css({ height: $(document).height()});
+  $(window).resize(function(){
+    $("#nav").css({ height: $(window).height()});
+    $("#nav").css({ height: $(document).height()});
+  });
+});
+
+// $("#main").click(function() {
+// //Hide the menus if visible
+// });
+
+
+// function(){
+//   $(this).animate({width:'75px'},400);
+//   $(".category-title").animate({"padding-left":"75px"},400);
+//   $("#name").animate({"padding-left":"25%"},400);
+//   $("#name").animate({"padding-left":"25%"},400);
+// }).trigger('mouseleave');
+// });
 
 clickLocations = [];
 
@@ -78,162 +105,3 @@ function logClicks(x,y) {
 $(document).click(function(loc) {
   // your code goes here!
 });
-
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
-var map;    // declares a global map variable
-
-
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
-function initializeMap() {
-
-  var locations;
-
-  var mapOptions = {
-    disableDefaultUI: true
-  };
-
-  /*
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js.
-  */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
-
-
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
-
-    // initializes an empty array
-    var locations = [];
-
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
-
-    // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
-
-    // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
-
-    return locations;
-  }
-
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
-
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
-
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    });
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
-
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
-
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
-  function pinPoster(locations) {
-
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
-      // the search request object
-      var request = {
-        query: place
-      };
-
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
-    });
-  }
-
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
-
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
-
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
-
-}
-
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
-// // Calls the initializeMap() function when the page loads
-// window.addEventListener('load', initializeMap);
-//
-// // Vanilla JS way to listen for resizing of the window
-// // and adjust map bounds
-// window.addEventListener('resize', function(e) {
-//   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-// });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,14 +3,55 @@
 var bio = {
   "name" : "Daniel Chang",
   "role" : "Web Developer",
-  "contacts" : {
-    "email" : "chang.daniel@gmail.com",
-    "phone" : "714 263 6819",
-    "location" : "San Jose, CA"
-  },
-  "pictureURL" : "images/fry.jpg",
   "welcomeMessage" : "Welcome!",
-  "skills" : ["Ruby", "Javascript", "HTML", "CSS", "R"]
+  "skills" : [
+    {
+      "value" : "Javascript",
+      "icon" : "images/js_200x200.png"
+    },
+    {
+      "value" : "HTML",
+      "icon" : "images/html_512x512.png"
+    },
+    {
+      "value" : "CSS",
+      "icon" : "images/css_24x24.png"
+    },
+    {
+      "value" : "SQL",
+      "icon" : "images/sql_128x128.png"
+    },
+    {
+      "value" : "Ruby",
+      "icon" : "images/ruby_995x996.png"
+    },
+    {
+      "value" : "R/R Studio",
+      "icon" : "images/r_64x64.png"
+    }
+  ],
+  "contacts" : [
+    {
+      "type" : "email",
+      "value" : "chang.daniel@gmail.com",
+      "icon" : "images/mail_24x24.png"
+    },
+    {
+      "type" : "phone",
+      "value" : "714 263 6819",
+      "icon" : "images/phone_24x24.png"
+    },
+    {
+      "type" : "linkedin",
+      "value" : "ccc",
+      "icon" : "images/linkedin_24x24.png"
+    },
+    {
+      "type" : "github",
+      "value" : "cccddd",
+      "icon" : "images/github_24x24.png"
+    }
+  ]
 };
 
 var projects = {
@@ -40,7 +81,13 @@ var work = {
       "employer" : "Machine Zone",
       "dates" : "July 2014  to Mar 2016",
       "location" : "Palo Alto, CA",
-      "description" : "lormemasdfm"
+      "description" : "<ul>Guide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operationGuide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operation</ul>"
+      // [
+      //   "Guide and implement targeted events tool to optimize value and LTV of customer audience along with increase safety of operation",
+      //   "Establish post acquisition funnel to increase conversion rates and reduce time to step up conversion",
+      //   "Establish & lead a 24/7 live operations team tasked with maximizing hour by hour operations for top grossing mobile game, Game of War",
+      //   "Split test and analyze various events and sales to determine best action for driving specific KPI among specific customer audience"
+      // ]
     },
     {
       "title" : "Country Manager",
@@ -48,7 +95,12 @@ var work = {
       "employer" : "gamigo Inc",
       "dates" : "Mar 2013 to May 2014",
       "location" : "San Francisco, CA",
-      "description" : "lormemasdfm"
+      "description" : "adsfasdf"
+      // [
+      //   "Establish San Francisco office for gamigo Inc; including set up of lease agreement, office infrastructure, and HR policies",
+      //   "Oversee the product and team migration from Outspark to gamigo Inc post acquisition deal",
+      //   "Lead live ops team in improving customer lifetime value via retention and monetization activities"
+      // ]
     },
     {
       "title" : "Director of Product & Sales",
@@ -56,7 +108,11 @@ var work = {
       "employer" : "Outspark Inc",
       "dates" : "Dec 2010 to Mar 2013",
       "location" : "San Francisco, CA",
-      "description" : "lormemasdfm"
+      "description" : "Plan, project lead, and bring into production automated CRM program to aimed at improving marketing funnel and lifetime value of customers <br> MySQL queries to evaluate marketing partner performance at multiple checkpoints along funnel"
+      // [
+      //   "Plan, project lead, and bring into production automated CRM program to aimed at improving marketing funnel and lifetime value of customers",
+      //   "MySQL queries to evaluate marketing partner performance at multiple checkpoints along funnel"
+      // ]
     }
   ]
 }
@@ -86,7 +142,7 @@ var education = {
   ]
 }
 
-$("#main").append(internationalizeButton)
+// $("#main").append(internationalizeButton)
 
 function inName(str) {
   var words = str.split(" ");
@@ -97,14 +153,26 @@ function inName(str) {
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// $("#name").prepend(formattedRole);
+$("#name").prepend(formattedName);
 
 skills.display = function() {
   if (bio.skills.length > 0) {
     for (skill in bio.skills) {
-      var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-      $("#skills").append(formattedSkill);
+      var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill].value);
+      formattedSkill = formattedSkill.replace("%icon%", bio.skills[skill].icon);
+      $("#skills-list").append(formattedSkill);
+    }
+  }
+};
+
+contacts.display = function() {
+  if (bio.contacts.length > 0) {
+    for (contact in bio.contacts) {
+      var formattedContact = HTMLcontact.replace("%type%", bio.contacts[contact].type);
+      formattedContact = formattedContact.replace("%icon%", bio.contacts[contact].icon);
+      formattedContact = formattedContact.replace("%value%", bio.contacts[contact].value);
+      $("#contact-list").append(formattedContact);
     }
   }
 };
@@ -139,7 +207,7 @@ education.display = function() {
     var formattedEducation = formattedDates + formattedName + formattedLocation + formattedDegree + formattedMajor;
     $(".education-entry:last").append(formattedEducation);
   }
-}
+};
 
 work.display = function() {
   for (job in work.jobs) {
@@ -152,8 +220,12 @@ work.display = function() {
     var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
     var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
     var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    var fomrattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    var formattedEmployerTitle = formattedLocation + formattedEmployer + formattedDates + formattedTitle + fomrattedDescription;
+    // var formattedList = ""
+    // for (var n = 0; n < work.jobs[job].description.length; n++) {
+    //   formattedList += HTMLworkList.replace("%data%",work.jobs[job].description[n]);
+    // }
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    var formattedEmployerTitle = formattedLocation + formattedEmployer + formattedDates + formattedTitle + formattedDescription;
     $(".work-entry:last").append(formattedEmployerTitle);
   };
 }
@@ -169,6 +241,4 @@ education.display();
 projects.display();
 work.display();
 skills.display();
-
-
-// $("#mapDiv").append(googleMap);
+contacts.display();
